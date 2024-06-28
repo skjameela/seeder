608d3360b6787b95e8fbb781ddd2cbee3cfba7d7
skjameela <shaik.jameela@zemosolabs.com>
2024-06-28 16:58:33 +0530

    created buttom component

--- WebpackReactTemplate/.storybook/main.ts
+++ WebpackReactTemplate/.storybook/main.ts
@@ -1,28 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
-import path from 'path'
-import webpackConfig from '../webpack.config'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-  },
-  webpackFinal: (config) => {
-    if (!config.resolve) config.resolve = {}
-    config.resolve.alias = {
-      ...config.resolve?.alias,
-      ...webpackConfig.resolve.alias,
-    }
-    return config
   },
 }
 export default config
--- WebpackReactTemplate/src/theme/index.tsx
+++ WebpackReactTemplate/src/theme/index.tsx
@@ -1,206 +1,297 @@
-import { SxProps, createTheme } from '@mui/material/styles'
-import { PALETTE } from './themeConstant'
-declare module '@mui/material/styles' {
-  interface TypeText {
-    highEmphasis?: string
-    mediumEmphasis?: string
-    lowEmphasis?: string
-  }
+import { createTheme } from "@mui/material/styles";
 
-  interface Palette {
-    gamma: {
-      SUCCESS_100: string
-      SUCCESS_500: string
-      WARNING_100: string
-      WARNING_300: string
-      ERROR_100: string
-      ERROR_300: string
-      GREY_50: string
-      GREY_100: string
-      GREY_300: string
-      GREY_500: string
-      GREY_700: string
-      GREY_WHITE: string
-    }
-  }
-
-  interface PaletteOptions {
-    gamma: {
-      SUCCESS_100: string
-      SUCCESS_500: string
-      WARNING_100: string
-      WARNING_300: string
-      ERROR_100: string
-      ERROR_300: string
-      GREY_50: string
-      GREY_100: string
-      GREY_300: string
-      GREY_500: string
-      GREY_700: string
-      GREY_WHITE: string
-    }
-  }
-
-  interface PaletteColor {
-    '100': string
-    '300': string
-    '500': string
-    '700': string
-    '900': string
-  }
-
+declare module "@mui/material/styles" {
   interface TypographyVariants {
-    heading4: SxProps
-    heading6: SxProps
-    subtitle1: SxProps
-    subtitle2: SxProps
-    body1: SxProps
-    body2: SxProps
-    caption1: SxProps
-    caption2: SxProps
-    overline: SxProps
+    title: React.CSSProperties;
+    heading2: React.CSSProperties;
+    heading3: React.CSSProperties;
+    body1: React.CSSProperties;
+    body2: React.CSSProperties;
+    caption: React.CSSProperties;
+    button: React.CSSProperties & {
+      textTransform?: "none" | "capitalize" | "uppercase" | "lowercase";
+    };
   }
 
+  // Allows the use of the custom variants in the theme configuration
   interface TypographyVariantsOptions {
-    heading4: SxProps
-    heading6: SxProps
-    subtitle1: SxProps
-    subtitle2: SxProps
-    body1: SxProps
-    body2: SxProps
-    caption1: SxProps
-    caption2: SxProps
-    overline: SxProps
+    title?: React.CSSProperties;
+    heading2?: React.CSSProperties;
+    heading3?: React.CSSProperties;
+    body1?: React.CSSProperties;
+    body2?: React.CSSProperties;
+    caption?: React.CSSProperties;
+    button?: React.CSSProperties & {
+      textTransform?: "none" | "capitalize" | "uppercase" | "lowercase";
+    };
+  }
+
+  // Extending the existing Palette interface
+  interface TypeBackground {
+    default: string;
+    paper: string;
+    elevation0: string;
+    elevation1: string;
+    elevation2: string;
+  }
+
+  interface TypeText {
+    primary: string;
+    secondary: string;
+    disabled: string;
+    highEmphasis: string;
+    mediumEmphasis: string;
+    lowEmphasis: string;
+  }
+
+  // Palette Start
+  interface Palette {
+    primary: PaletteColor;
+    background: TypeBackground;
+    text: TypeText;
+    divider: string;
+    structural: PaletteStructural;
+    Borders: PaletteBorders;
+    Icon: PaletteIcon;
+  }
+
+  // Detailed interfaces for each custom property
+  interface PaletteColor {
+    main: string;
+    light: string;
+    dark: string;
+    contrastText: string;
+    purple: {
+      500: string;
+      400: string;
+      600: string;
+    };
+    white: {
+      500: string;
+    };
+  }
+
+  interface PaletteColorOptions {
+    main?: string;
+    light?: string;
+    dark?: string;
+    contrastText?: string;
+    purple?: {
+      500?: string;
+      400?: string;
+      600?: string;
+    };
+    white?: {
+      500?: string;
+    };
+  }
+
+  interface PaletteStructural {
+    background: string;
+    grey: {
+      100: string;
+    };
+  }
+
+  interface PaletteStructuralOptions {
+    background?: string;
+    grey?: {
+      100?: string;
+    };
+  }
+
+  interface PaletteBorders {
+    highEmphasis: string;
+    lowEmphasis: string;
+  }
+
+  interface PaletteBordersOptions {
+    highEmphasis?: string;
+    lowEmphasis?: string;
+  }
+
+  interface PaletteIcon {
+    highEmphasis: string;
+    lowEmphasis: string;
+  }
+
+  interface PaletteIconOptions {
+    highEmphasis?: string;
+    lowEmphasis?: string;
   }
 }
-declare module '@mui/material/Typography' {
+
+// Extend the TypographyProps to include the custom variants
+declare module "@mui/material/Typography" {
   interface TypographyPropsVariantOverrides {
-    heading4: true
-    heading6: true
-    subtitle1: true
-    subtitle2: true
-    body1: true
-    body2: true
-    caption1: true
-    caption2: true
-    overline: true
+    title: true;
+    heading2: true;
+    heading3: true;
+    body1: true;
+    body2: true;
+    caption: true;
+    button: true;
   }
 }
 
+// Define custom typography
+const typography = {
+  fontFamily: '"Gilroy", "Helvetica", "Arial", sans-serif',
+
+  // Title
+  title: {
+    fontSize: "36px", // 36px
+    fontWeight: 700, // Bold
+    lineHeight: "42px",
+    letterSpacing: "-0.015em",
+  },
+
+  // Heading 2
+  heading2: {
+    fontSize: "24px", // 24px
+    fontWeight: 600, // Bold
+    lineHeight: "29.4px",
+    letterSpacing: "-0.005em",
+  },
+
+  // Heading 3
+  heading3: {
+    fontSize: "18px", // 18px
+    fontWeight: 500, // Medium
+    lineHeight: "27px",
+  },
+
+  // Body 1
+  body1: {
+    fontSize: "16px", // 16px
+    fontWeight: 500, // Medium
+    lineHeight: "22.4px",
+  },
+
+  // Body 2
+  body2: {
+    fontSize: "14px", // 14px
+    fontWeight: 600, // Semibold
+    lineHeight: "17.15px",
+    letterSpacing: "0.01em",
+  },
+
+  // Caption
+  caption: {
+    fontSize: "12px", // 12px
+    fontWeight: 500, // Medium
+    lineHeight: "14.56px",
+    letterSpacing: "0.02em",
+  },
+
+  // Button
+  button: {
+    fontSize: "16px", // 16px
+    fontWeight: 600, // Semibold
+    lineHeight: "19px",
+    textTransform: "none" as 'none', // Ensures buttons do not transform text to uppercase
+  },
+};
+
+// Define custom palette
+const palette = {
+  primary: {
+    main: "#6C5DD3",
+    light: "#B4A9FF",
+    dark: "#393552",
+    contrastText: "#E8E8E9",
+    purple: {
+      500: "#6C5DD3",
+      400: "#B4A9FF",
+      600: "#393552",
+    },
+    white: {
+      500: "#E8E8E9",
+    },
+  },
+  text: {
+    primary: "#fff",
+    secondary: "#f22",
+    disabled: "#000",
+    highEmphasis: "#E8E7F0", // text/high emphasis
+    mediumEmphasis: "#C9C8CC", // text/medium emp
+    lowEmphasis: "#A5A5A6", // text/low emp
+  },
+  background: {
+    paper: "#fff",
+    default: "#000",
+    elevation0: "#19181C", // background/elevation 0
+    elevation1: "#201F24", // background/elevation 1
+    elevation2: "#2D2D30", // background/elevation 2
+  },
+  divider: "#393552", // divider color
+  structural: {
+    background: "#19181C", // Structural Colors/Backgrounds
+    grey: {
+      100: "#262529", // Structural Colors/greys
+    },
+  },
+  Borders: {
+    highEmphasis: "#413F4D", // Borders/high emp
+    lowEmphasis: "#28272B", // Borders/low emp
+  },
+  Icon: {
+    highEmphasis: "#D4D2DE", // Icon/high emp
+    lowEmphasis: "#727080", // Icon/low emp
+  },
+};
+
+// Define the theme
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: PALETTE.PRIMARY_500,
-      100: PALETTE.PRIMARY_100,
-      300: PALETTE.PRIMARY_300,
-      500: PALETTE.PRIMARY_500,
-      700: PALETTE.PRIMARY_700,
-      900: PALETTE.PRIMARY_900,
-    },
-    gamma: {
-      SUCCESS_100: PALETTE.GAMMA_SUCCESS_100,
-      SUCCESS_500: PALETTE.GAMMA_SUCCESS_500,
-      WARNING_100: PALETTE.GAMMA_WARNING_100,
-      WARNING_300: PALETTE.GAMMA_WARNING_300,
-      ERROR_100: PALETTE.GAMMA_ERROR_100,
-      ERROR_300: PALETTE.GAMMA_ERROR_300,
-      GREY_50: PALETTE.GAMMA_GREY_50,
-      GREY_100: PALETTE.GAMMA_GREY_100,
-      GREY_300: PALETTE.GAMMA_GREY_300,
-      GREY_500: PALETTE.GAMMA_GREY_500,
-      GREY_700: PALETTE.GAMMA_GREY_700,
-      GREY_WHITE: PALETTE.GAMMA_GREY_WHITE,
-    },
-    text: {
-      lowEmphasis: PALETTE.TEXT_LOW_EMPHASIS,
-      mediumEmphasis: PALETTE.TEXT_MEDIUM_EMPHASIS,
-      highEmphasis: PALETTE.TEXT_HIGH_EMPHASIS,
-    },
-  },
+  spacing: [0, 4, 8, 12, 16, 20, 24, 32],
 
-  typography: {
-    fontFamily: ['Graphik'].join(','),
-    heading4: {
-      fontSize: '40px',
-      fontWeight: 500,
-      fontStyle: 'normal',
-      lineHeight: '54px',
-      textTransform: 'none',
-      letterSpacing: '-.4px',
-    },
-    heading6: {
-      fontSize: '24px',
-      fontWeight: 400,
-      fontStyle: 'normal',
-      lineHeight: '34px',
-      textTransform: 'none',
-    },
-    subtitle1: {
-      fontSize: '20px',
-      fontWeight: 500,
-      fontStyle: 'normal',
-      lineHeight: '28px',
-      textTransform: 'none',
-      letterSpacing: '.1px',
-    },
-    subtitle2: {
-      fontSize: '20px',
-      fontWeight: 400,
-      fontStyle: 'normal',
-      lineHeight: '28px',
-      textTransform: 'none',
-      letterSpacing: '.1px',
-    },
-    body1: {
-      fontSize: '16px',
-      fontWeight: 500,
-      fontStyle: 'normal',
-      lineHeight: '22px',
-      letterSpacing: '.16px',
-      textTransform: 'none',
-    },
-    body2: {
-      fontSize: '16px',
-      fontWeight: 400,
-      fontStyle: 'normal',
-      lineHeight: '22px',
-      letterSpacing: '.16px',
-      textTransform: 'none',
-    },
-    button: {
-      fontSize: '14px',
-      fontWeight: 500,
-      fontStyle: 'normal',
-      lineHeight: '42px',
-      letterSpacing: '.14px',
-      textTransform: 'none',
-    },
-    caption1: {
-      fontSize: '14px',
-      fontWeight: 500,
-      fontStyle: 'normal',
-      lineHeight: '16px',
-      letterSpacing: '.14px',
-      textTransform: 'none',
-    },
-    caption2: {
-      fontSize: '14px',
-      fontWeight: 400,
-      fontStyle: 'normal',
-      lineHeight: '16px',
-      letterSpacing: '.14px',
-      textTransform: 'none',
-    },
-    overline: {
-      fontSize: '12px',
-      fontWeight: 400,
-      fontStyle: 'normal',
-      lineHeight: '14px',
-      letterSpacing: '.06px',
-      textTransform: 'none',
-    },
-  },
-  spacing: 4,
-})
+  typography,
+  palette,
 
-export default theme
+  components: {
+    // Customize MUI Button component styles
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          // You can add general styles that apply to all buttons here
+          fontSize: "16px",
+          lineHeight: "19px",
+          textAlign: "center",
+          borderRadius: "12px",
+          color: palette.primary.white["500"],
+        },
+        sizeSmall: {
+          // Styles for small buttons
+          padding: "6px 12px",
+        },
+        sizeLarge: {
+          // Styles for large buttons
+          padding: "20px 40px",
+        },
+        containedPrimary: {
+          backgroundColor: palette.primary.purple["500"], // Purple Primary Button
+          padding: "12px 24px",
+        },
+        containedSecondary: {
+          backgroundColor: palette.primary.white["500"], // White Primary Button
+          color: "#201F24",
+          padding: "12px 24px",
+        },
+        outlined: {
+          borderColor: "#E8E8E9", // Outlined Button
+          color: "#E8E8E9",
+          border: "1px solid #E8E8E9",
+          padding: "12px 24px",
+        },
+        text: {
+          color: "#B4A9FF", // Text Button
+          padding: "4px 12px",
+          fontWeight: "600",
+        },
+      },
+    },
+    // Customize other components as needed
+  },
+});
+
+export default theme;
